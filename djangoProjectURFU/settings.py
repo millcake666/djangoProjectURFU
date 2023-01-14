0e9bd9265f2c4bd8ec0cee8322d0de6b5baa4b3e
millcake666 <53191191+millcake666@users.noreply.github.com>
2023-01-15 00:04:32 +0500

    allowed hosts

--- djangoProjectURFU/settings.py
+++ djangoProjectURFU/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4swjbwh6u9=89fi8co#(n+xa^een%wm08pyo^znvz&_o8b=8il
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
